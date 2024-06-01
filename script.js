cd2d7d678ed2b225e62bcc1c9afaf63cd4643b73
kaniadhea <kaniadhea7@gmail.com>
2024-06-01 22:27:30 +0700

    untuk menampilkan data

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 document.getElementById('surveyForm').addEventListener('submit', function(event) {
     event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const dob = document.getElementById('dob').value;
