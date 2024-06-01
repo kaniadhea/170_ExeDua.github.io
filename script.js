b1ed9bba62ba25b815aa9255fde94d1a047233b3
kaniadhea <kaniadhea7@gmail.com>
2024-06-01 22:28:39 +0700

    untuk mendapatkan nilai dari elemen input gender

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ document.getElementById('surveyForm').addEventListener('submit', function(event)
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const dob = document.getElementById('dob').value;
+    const gender = document.querySelector('input[name="gender"]:checked') ? document.querySelector('input[name="gender"]:checked').value : 'Not specified';
+    
