e59139a9a4db8d58e08feeadbf2468042a06cd79
kaniadhea <kaniadhea7@gmail.com>
2024-06-01 22:29:38 +0700

    untuk menampilkan data

--- script.js
+++ script.js
@@ -5,4 +5,5 @@ document.getElementById('surveyForm').addEventListener('submit', function(event)
     const email = document.getElementById('email').value;
     const dob = document.getElementById('dob').value;
     const gender = document.querySelector('input[name="gender"]:checked') ? document.querySelector('input[name="gender"]:checked').value : 'Not specified';
-    
+    const country = document.getElementById('country').value;
+    const feedback = document.getElementById('feedback').value
